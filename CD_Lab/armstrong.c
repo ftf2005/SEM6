aa57d2339e7cab4e997a5b2325d46e7a5fa16352
ftf2005 <frickthefasak2@outlook.com>
2024-12-02 14:59:07 +0530

    Create armstrong.c

---
+++ CD_Lab/armstrong.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+int main()
+{
+	int n,r,z,x,y,sum=0,count=0;
+	printf("enter the value for the number:");
+	scanf("%d",&n);
+	x=y=n;
+	while(x!=0)
+	{
+		r=x%10;
+		count++;
+		x=x/10;
+	}
+	
+	while(y!=0)
+	{
+		z=y%10;
+		sum=sum+(z*z*z);
+		y=y/10;
+	}
+	if(sum==n)
+	{
+		printf("its a armstrong number");
+	}
+	else
+	{
+		printf("its not a armstrong number");
+	}
+}
