c2df8ec03b40d25401069a0ca9914c32a64d742d
ftf2005 <frickthefasak2@outlook.com>
2024-12-02 15:09:43 +0530

    Update armstrong.c

--- CD_Lab/armstrong.c
+++ CD_Lab/armstrong.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "math.h"
 int main()
 {
 	int n,r,z,x,y,sum=0,count=0;
@@ -15,7 +16,7 @@ int main()
 	while(y!=0)
 	{
 		z=y%10;
-		sum=sum+(z*z*z);
+		sum += pow(z, count);
 		y=y/10;
 	}
 	if(sum==n)
@@ -26,4 +27,6 @@ int main()
 	{
 		printf("its not a armstrong number");
 	}
+//	printf("%d\n",sum);
+//	printf("%d\n",count);
 }
