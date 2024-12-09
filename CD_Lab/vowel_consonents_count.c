d0025031b4b1a53d31d07599a2ad4b0c23d89e4a
ftf2005 <frickthefasak2@outlook.com>
2024-12-09 15:28:17 +0530

    Update vowel_consonents_count.c

--- CD_Lab/vowel_consonents_count.c
+++ CD_Lab/vowel_consonents_count.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
+#include <ctype.h>
+
 int main() {
-    char input[100];
+    char ch;
     int vowels = 0, consonants = 0;
-    int i = 0;
-    printf("Enter a string: ");
-    scanf("%s", input);  
-    while (input[i] != '\0') {
-        char ch = input[i];
-        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                vowels++;
-            } else {
-                consonants++;
-            }
+    int prev_ch = 0;
+
+    printf("Enter a paragraph (press Enter twice to end input):\n");
+
+    while (1) {
+        ch = getchar();
+
+        if (ch == '\n' && prev_ch == '\n') {
+            break;
         }
-        i++; 
+
+        prev_ch = ch;
+
+        ch = tolower(ch);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            vowels++;
+        } else if (ch >= 'a' && ch <= 'z') {
+            consonants++;
+        }
     }
+
     printf("Vowels: %d\n", vowels);
     printf("Consonants: %d\n", consonants);
+
     return 0;
 }
