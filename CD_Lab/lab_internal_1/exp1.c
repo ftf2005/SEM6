518b1b187a08bb9f16af4e0b3d754aed178e623d
ftf2005 <frickthefasak2@outlook.com>
2024-12-30 15:34:24 +0530

    Update exp1.c

--- CD_Lab/lab_internal_1/exp1.c
+++ CD_Lab/lab_internal_1/exp1.c
@@ -2,88 +2,55 @@
 #include <ctype.h>
 #include <string.h>
 
-void main() {
+int main() {
     char str[20];
-    int state, i = 1;
+    int state, i = 0;
+    
+    // Get the input string
     printf("Enter the string: ");
     fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0';
+    str[strcspn(str, "\n")] = '\0';  // Remove trailing newline
 
-    if (isalpha(str[0])) {
-        state = 1;
-    } else if (isdigit(str[0])) {
-        state = 2;
-    } else {
-        state = 10;
-    }
+    // Initial state based on the first character
+    if (isalpha(str[0])) state = 1;   // Variable
+    else if (isdigit(str[0])) state = 2;  // Integer constant
+    else state = 10;  // Error
 
-    while (str[i] != '\0') {
+    // Process the string character by character
+    while (str[i] != '\0' && state != 10) {
         switch (state) {
-            case 1:
-                if (isalpha(str[i]) || isdigit(str[i])) {
-                    state = 1;
-                } else if (str[i] == '(') {
-                    state = 3;
-                } else if (str[i] == '[') {
-                    state = 5;
-                } else {
-                    state = 10;
-                }
+            case 1: // Variable
+                if (isalnum(str[i]) || str[i] == '(' || str[i] == '[') state = 1;
+                else if (str[i] == ')') state = 6;  // Function
+                else if (str[i] == ']') state = 7;  // Array
+                else state = 10;  // Error
                 break;
-            case 2:
-                if (isdigit(str[i])) {
-                    state = 2;
-                } else if (str[i] == '.' && isdigit(str[i + 1])) {
-                    state = 4;
-                } else {
-                    state = 10;
-                }
+            case 2: // Integer constant
+                if (isdigit(str[i])) state = 2;
+                else if (str[i] == '.' && isdigit(str[i+1])) state = 4; // Real constant
+                else state = 10;
                 break;
-            case 3:
-                if (str[i] == ')' && str[i + 1] == '\0') {
-                    state = 6;
-                } else {
-                    state = 10;
-                }
+            case 4: // Real constant
+                if (isdigit(str[i])) state = 4;
+                else state = 10;
                 break;
-            case 4:
-                if (isdigit(str[i])) {
-                    state = 4;
-                } else {
-                    state = 10;
-                }
+            case 6: // Function
+                if (str[i] != ')') state = 10;
                 break;
-            case 5:
-                if (isdigit(str[i])) {
-                    state = 5;
-                } else if (str[i] == ']' && str[i + 1] == '\0') {
-                    state = 7;
-                } else {
-                    state = 10;
-                }
-                break;
-            default:
-                state = 10;
+            case 7: // Array
+                if (str[i] != ']') state = 10;
                 break;
         }
         i++;
-        if (state == 10) {
-            break;
-        }
     }
 
-    printf("%s", str);
-    if (state == 1) {
-        printf(" Input is a variable.\n");
-    } else if (state == 2) {
-        printf(" Input is an integer constant.\n");
-    } else if (state == 4) {
-        printf(" Input is a real constant.\n");
-    } else if (state == 6) {
-        printf(" Input is a function.\n");
-    } else if (state == 7) {
-        printf(" Input is an array.\n");
-    } else {
-        printf(" There is an error in the given expression.\n");
-    }
+    // Output based on the final state
+    if (state == 1) printf("Input is a variable.\n");
+    else if (state == 2) printf("Input is an integer constant.\n");
+    else if (state == 4) printf("Input is a real constant.\n");
+    else if (state == 6) printf("Input is a function.\n");
+    else if (state == 7) printf("Input is an array.\n");
+    else printf("There is an error in the given expression.\n");
+
+    return 0;
 }
