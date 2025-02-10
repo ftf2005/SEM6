2350202c5cffdb357495afbedfdc03d3331e0d7d
ftf2005 <frickthefasak2@outlook.com>
2025-02-10 15:00:16 +0530

    Update feb10_RDP_code_2.c

--- CD_Lab/feb10_RDP_code_2.c
+++ CD_Lab/feb10_RDP_code_2.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
-
 int i = 0, t = 1;
 char str[20];
 
@@ -12,21 +10,21 @@ void F();
 void error();
 
 void main() {
-  
     printf("The given grammar is:\n");
-    printf("\nE -> E+T/T");
-    printf("\nT -> T*F/F");
-    printf("\nF -> F*/a/b");
-
-    printf("\nEnter the string to be parsed: ");
-    scanf("%s",str);
-    E();
-    if (t != 1 || str[i] != '\0') {
-        printf("\nGiven string is not accepted");
-    } else {
-        printf("\nThe given string is accepted");
-    }
+    printf("\nE -> E+T | T");
+    printf("\nT -> TF | F");
+    printf("\nF -> F* | a | b");
     
+    printf("\nEnter the string to be parsed: ");
+    scanf("%s", str);
+    
+    E();
+    
+    if (t != 1 || str[i] != '\0') {
+        printf("\nGiven string is not accepted\n");
+    } else {
+        printf("The given string is accepted\n");
+    }
 }
 
 void E() {
@@ -36,10 +34,6 @@ void E() {
 
 void EPRIME() {
     if (str[i] == '+') {
-        i++;
-        T();
-        EPRIME();
-    } else if (str[i] == '/') {
         i++;
         T();
         EPRIME();
@@ -54,14 +48,9 @@ void T() {
 }
 
 void TPRIME() {
-    if (str[i] == '*') {
+    if (str[i] == 'F') {   
         i++;
         F();
-        TPRIME();
-    } else if (str[i] == '/') {
-        i++;
-        F();
-        TPRIME();
     } else {
         return;
     }
@@ -70,14 +59,9 @@ void TPRIME() {
 void F() {
     if (str[i] == 'a' || str[i] == 'b') {
         i++;
-    } else if (str[i] == '(') {
+    } else if (str[i] == '*') {
         i++;
-        E();
-        if (str[i] == ')') {
-            i++;
-        } else {
-            error();
-        }
+        F();
     } else {
         error();
     }
